160395721240078671853321f9b5c5faa7e2b57e
BSarahB <bslajta@gmail.com>
2019-10-29 02:04:54 +0100

    adding files

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,13 @@
+class WelcomeController < ApplicationController
+  def show
+  end
+
+  def show_gossip
+  end
+
+  def display_gossip
+  end
+
+  def display_user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/show'
+  get 'welcome/show_gossip'
+  get 'welcome/display_gossip'
+  get 'welcome/display_user'
+  get 'contact/index'
+  get 'team/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
